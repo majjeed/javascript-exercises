af6ca8f718a10606d44f15bee512d9bd955f8d37
majjeed <abdulmajeed.hersi@gmail.com>
2024-03-26 23:21:16 +0000

    fix tests 1 to 6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let args = [...arguments].slice(1);
+    let arr = [...arguments][0];
+    let filtered = [];
+    // for (let i = 0; i < args.length; i++) {
+    //     for (let toRemove of args) {
 
+    //     }
+    // }
+    let i = 0;
+    do {
+        for (let element of arr) {
+            if (element != args[i]) {
+                filtered.push(element);
+            }
+        }
+        arr = filtered;
+        filtered = [];
+        i++;
+    } while (i < args.length);
+    return arr;
 };
 
 // Do not edit below this line
