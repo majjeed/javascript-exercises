6a697662bbad48dbfe29601150fed98789fa06ae
majjeed <abdulmajeed.hersi@gmail.com>
2024-03-27 19:01:58 +0000

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0) return 'ERROR';
+    else if (typeof a != 'number' || typeof b != 'number') return 'ERROR';
+    else if (a > b) {
+        const temp = a;
+        a = b;
+        b = temp;
+    }
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
