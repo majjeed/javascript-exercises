a9b41e1fadb22c68563eb32e74d72244cc24756f
majjeed <abdulmajeed.hersi@gmail.com>
2024-04-17 12:21:10 +0100

    complete 12_findTheOldest
    Using ternary operator instead

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((prevPerson, currPerson) => {
+        let currYear = (new Date).getFullYear();
+        let prevPersonDeath = 'yearOfDeath' in prevPerson ? prevPerson.yearOfDeath : currYear;
+        let currPersonDeath = 'yearOfDeath' in currPerson ? currPerson.yearOfDeath : currYear;
+        return (prevPersonDeath - prevPerson.yearOfBirth) > (currPersonDeath - currPerson.yearOfBirth) ?
+                prevPerson : currPerson;
+    })
 };
 
 // Do not edit below this line
