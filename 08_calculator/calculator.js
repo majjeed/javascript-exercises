5e2de50a597b53109bcc4bad952b559a1fa44e13
majjeed <abdulmajeed.hersi@gmail.com>
2024-04-17 10:34:25 +0100

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prev, curr) => prev * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	return a === 0 || a === 1 ? 1:
+  a === 2 ? 2 : factorial(a-1) * a; 
 };
 
 // Do not edit below this line
