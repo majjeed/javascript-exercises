34e1c4e406af0a01dd1e28c21cea68dcd5abe2a4
majjeed <abdulmajeed.hersi@gmail.com>
2024-04-17 11:57:02 +0100

    complete 10_fibonacci
    Using recursion instead of for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    fib = +fib;
+    if (fib === 0) return 0;
+    if (fib < 0) return 'OOPS';
+    return fib === 1 || fib === 2 ? 1:
+            fibonacci(fib-1) + fibonacci(fib - 2);
 };
 
 // Do not edit below this line
